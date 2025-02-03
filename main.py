84aafd788f768fdfdca6798d079107cb15ae24e5
Tom Reiertsen <tom@reiertsen.com>
2025-02-03 17:57:02 +0100

    Completed Bookbot

--- main.py
+++ main.py
@@ -2,14 +2,40 @@ def count_words(text):
     words = text.split()
     return len(words)    
 
+def count_chars(text):
+    r = {}
+    l = text.lower()
+    for c in l:
+        if not c in r:
+            r[c] = 1
+        else:
+            r[c] = r[c] + 1
+    
+    return r
+
 def main():
     wordCount = 0
+    charCount = { }
 
     with open("books/frankenstein.txt") as f:
         for file_contents in f:
             wordCount += count_words(file_contents)
-            print(file_contents.strip())
+            chars = count_chars(file_contents)
+            for c in chars:
+                if not c in charCount:
+                    charCount[c] = chars[c]
+                else:
+                    charCount[c] = charCount[c] + chars[c]
+            
+            # print(file_contents.strip())
         
-    print(wordCount)
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{wordCount} words found in the document")
+    print()
+    for c in charCount:
+        if c in 'abcdefghijklmnopqstuvwxyz':
+            print(f"The '{c}' character was found {charCount[c]} times")
+    print("--- End report ---")
 
 main()
